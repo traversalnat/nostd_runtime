fd7751d9b779cd22b190341bae8f69c84c18cf45
traversalnat <1914619958@qq.com>
2022-11-02 15:09:31 +0800

    just use future

--- src/async_executor.rs
+++ src/async_executor.rs
@@ -10,45 +10,22 @@ use spin::Mutex;
 
 pub use futures::join;
 
-/// A spawned future and its current state.
-type Task = async_task::Task<()>;
+type PinBoxFuture = Pin<Box<dyn Future<Output = ()>>>;
 
-pub struct JoinHandle(async_task::JoinHandle<(), ()>);
-
-impl Future for JoinHandle {
-    type Output = ();
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        match Pin::new(&mut self.0).poll(cx) {
-            Poll::Pending => {
-                self.0.waker().wake();
-                Poll::Pending
-            }
-            Poll::Ready(output) => Poll::Ready(output.expect("task failed")),
-        }
-    }
-}
-
-pub(crate) type Queue = Arc<Mutex<LinkedList<Task>>>;
-pub(crate) type JoinHandleQueue = Arc<Mutex<LinkedList<JoinHandle>>>;
+pub(crate) type Queue = Arc<Mutex<LinkedList<PinBoxFuture>>>;
 
 /// Runtime definition
 pub(crate) struct Runtime {
     pub(crate) task_queue: Queue,
-    pub(crate) handle_queue: JoinHandleQueue,
 }
 
 impl Runtime {
-    pub fn task_pop_front(&self) -> Option<Task> {
+    pub fn task_pop_front(&self) -> Option<PinBoxFuture> {
         self.task_queue.lock().pop_front()
     }
 
-    pub fn handle_pop_front(&self) -> Option<JoinHandle> {
-        self.handle_queue.lock().pop_front()
-    }
-
-    pub fn handle_push_back(&self, handle: JoinHandle) {
-        self.handle_queue.lock().push_back(handle);
+    pub fn task_push_back(&self, task: PinBoxFuture) {
+        self.task_queue.lock().push_back(task)
     }
 }
 
@@ -60,7 +37,6 @@ impl Executor {
     pub fn new() -> Self {
         let runtime = Runtime {
             task_queue: Arc::new(Mutex::new(LinkedList::new())),
-            handle_queue: Arc::new(Mutex::new(LinkedList::new())),
         };
         Self { runtime }
     }
@@ -70,17 +46,7 @@ impl Executor {
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        let queue = self.runtime.task_queue.clone();
-        let (task, handle) = async_task::spawn(
-            future,
-            move |t| {
-                queue.lock().push_back(t);
-            },
-            (),
-        );
-
-        task.schedule();
-        self.runtime.handle_push_back(JoinHandle(handle));
+        self.runtime.task_push_back(Box::pin(future));
     }
 
     // one thread executor
@@ -90,36 +56,25 @@ impl Executor {
         let mut cx = Context::from_waker(&waker);
 
         let mut future = unsafe { Pin::new_unchecked(&mut future) };
-        let mut main_stop = false;
         loop {
-            if !main_stop {
-                match Future::poll(future.as_mut(), &mut cx) {
-                    Poll::Ready(_) => {
-                        main_stop = true;
-                    }
-                    Poll::Pending => {
-                        continue;
-                    }
-                };
-            }
+            match Future::poll(future.as_mut(), &mut cx) {
+                Poll::Ready(_) => {
+                    break;
+                }
+                Poll::Pending => {
+                    continue;
+                }
+            };
+        }
 
-            let len = self.runtime.handle_queue.lock().len();
-            if len == 0 {
-                break;
-            }
-
-            if let Some(task) = self.runtime.task_pop_front() {
-                task.run();
-            }
-
-            let mut handle = self.runtime.handle_pop_front().unwrap();
+        while let Some(mut handle) = self.runtime.task_pop_front() {
             let check_handle = unsafe { Pin::new_unchecked(&mut handle) };
             match Future::poll(check_handle, &mut cx) {
                 Poll::Ready(_) => {
                     continue;
                 }
                 Poll::Pending => {
-                    self.runtime.handle_push_back(handle);
+                    self.runtime.task_push_back(handle);
                 }
             };
         }
